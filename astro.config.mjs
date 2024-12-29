ba7f0c368b9a06782770328c0b0a678e83d9c02f
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-12-29 17:22:01 -0300

    Cambios

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import unocss from 'unocss/astro'
+import icon from "astro-icon";
+import expressiveCode from 'astro-expressive-code';
+import { BASE_URL } from './src/config'
+import { remarkPlugins, rehypePlugins } from './plugins';
+
+// https://astro.build/config
+export default defineConfig({
+  site: BASE_URL,
+  integrations: [
+    expressiveCode(),
+    mdx(),
+    sitemap({ filter: page => ['/tags/','/categories/'].map(PATH=>page.startsWith(BASE_URL+PATH)).includes(false) }),
+    unocss({ injectReset: true, configFile: '/uno.config.ts' }),
+    icon(),
+  ],
+  markdown: {
+    remarkPlugins,
+    rehypePlugins
+  }
+});
